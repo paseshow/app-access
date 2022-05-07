8e591aa252bede362506f468172ec9b4818fd5e8
rBariles <rbariles@chattigo.com>
2022-05-07 07:40:38 -0300

    Prueba push heroku

--- server.js
+++ server.js
@@ -6,14 +6,13 @@ const path = require('path');
 const app = express();
 
 // enable ssl redirect
-app.use(sslRedirect());
+app.use(sslRedirect.default);
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist'));
 
 app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname,'/dist/index.html'));
+    res.sendFile(path.join(__dirname,'/dist/index.html'));
 });
 
 // Start t< he app by listening on the default Heroku port
