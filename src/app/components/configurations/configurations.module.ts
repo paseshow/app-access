3c1e2f9f8b5a5123cc0828db95b1e3c7c6160d1e
rBariles <rbariles@chattigo.com>
2022-05-09 08:06:10 -0300

    Implements qr scanner angular 2

--- src/app/components/configurations/configurations.module.ts
+++ src/app/components/configurations/configurations.module.ts
@@ -5,7 +5,7 @@ import { DashboarComponent } from "../dashboar/dashboar.component";
 import { ConfigurationsComponent } from "./configurations.component";
 import { Route } from "./configurations.routing";
 import { ScanComponent } from "./scan/scan.component";
-import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { NgQrScannerModule } from "angular2-qrscanner";
 
 
 @NgModule({
@@ -17,7 +17,7 @@ import { ZXingScannerModule } from '@zxing/ngx-scanner';
     imports: [
         CommonModule,
         RouterModule.forChild(Route),
-        ZXingScannerModule
+        NgQrScannerModule
     ]
 })
 export class ConfigurationsModule {}
--- src/app/components/configurations/scan/scan.component.ts
+++ src/app/components/configurations/scan/scan.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { BarcodeFormat } from '@zxing/library';
+import { AfterViewChecked, AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { QrScannerComponent } from 'angular2-qrscanner';
 import { AccessControlService } from 'app/services/access-control.service';
 
 
@@ -8,7 +8,10 @@ import { AccessControlService } from 'app/services/access-control.service';
   templateUrl: './scan.component.html',
   styleUrls: ['./scan.component.css']
 })
-export class ScanComponent implements OnInit {
+export class ScanComponent implements OnInit , AfterViewInit{
+
+  @ViewChild(QrScannerComponent, { static: false })
+  qrScannerComponent: QrScannerComponent;
 
   qrCode: string = '';
   data = {
@@ -20,42 +23,99 @@ export class ScanComponent implements OnInit {
     fecha: ''
   };
 
-  torchEnabled = false;
-  tryHarder = false;
-
-  formatsEnabled: BarcodeFormat[] = [
-    BarcodeFormat.CODE_128,
-    BarcodeFormat.DATA_MATRIX,
-    BarcodeFormat.EAN_13,
-    BarcodeFormat.QR_CODE,
-  ];
+  stopAfterScan: boolean = false;
 
   constructor(
     private accessControlService: AccessControlService
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     //this.onScanSuccess("042A89F29D565E0E0F38E40D6CE5B86D");
+    
+
+    // this.qrScannerComponent.getMediaDevices().then(devices => {
+    //   console.log(devices);
+    //   const videoDevices: MediaDeviceInfo[] = [];
+    //   for (const device of devices) {
+    //     if (device.kind.toString() === 'videoinput') {
+    //       videoDevices.push(device);
+    //     }
+    //   }
+    //   if (videoDevices.length > 0) {
+    //     let choosenDev;
+    //     for (const dev of videoDevices) {
+    //       if (dev.label.includes('front')) {
+    //         choosenDev = dev;
+    //         break;
+    //       }
+    //     }
+    //     if (choosenDev) {
+    //       this.qrScannerComponent.chooseCamera.next(choosenDev);
+    //     } else {
+    //       this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
+    //     }
+    //   }
+    // });
+
+    
   }
 
-  onCodeResult(result: string) {
-    console.log("Scan Success -----------");
-    console.log(result);
-    this.qrCode = result;
+  ngAfterViewInit(): void {
+    navigator.mediaDevices.enumerateDevices().then( dev => {
+      console.log(dev)
 
-    this.accessControlService.accessByQrCode(result).subscribe(
-      (responseExit: any) => {
-        this.data = {
-          descuento: responseExit[0].descuento_sector_id.descripcion,
-          evento:  responseExit[0].sector_evento_id.evento_id.nombre,
-          fecha: 'hoy',
-          funcion: responseExit[0].sector_evento_id.descripcion,
-          ubicacionId: responseExit[0].id,
-          usuario: responseExit[0].reserva_id.cliente_id.nombre
+      const videoDevices: MediaDeviceInfo[] = [];
+      for (const device of dev) {
+        if (device.kind.toString() === 'videoinput') {
+          videoDevices.push(device);
         }
-      }, error => {
+      }
+      if (videoDevices.length > 0) {
+        let choosenDev;
+        for (const dev of videoDevices) {
+          if (dev.label.includes('front')) {
+            choosenDev = dev;
+            break;
+          }
+        }
+        if (choosenDev) {
+          this.qrScannerComponent.chooseCamera.next(choosenDev);
+        } else {
+          this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
+        }
+      }
+    });
+
+    
+
+    this.onCodeResult();
+  }
+
+  onCodeResult() {
+
+    this.qrScannerComponent.capturedQr.subscribe(result => {
+      this.stopAfterScan = true;
+      console.log("Scan Success -----------");
+      console.log(result);
+      this.qrCode = result;
+
+      this.accessControlService.accessByQrCode(result).subscribe(
+        (responseExit: any) => {
+          this.data = {
+            descuento: responseExit[0].descuento_sector_id.descripcion,
+            evento: responseExit[0].sector_evento_id.evento_id.nombre,
+            fecha: 'hoy',
+            funcion: responseExit[0].sector_evento_id.descripcion,
+            ubicacionId: responseExit[0].id,
+            usuario: responseExit[0].reserva_id.cliente_id.nombre
+          }
+        }, error => {
 
       });
+      this.stopAfterScan = false;
+    });
+    
 
 
   }
