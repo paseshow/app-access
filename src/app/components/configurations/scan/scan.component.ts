f0028b6418b710fb8ee466d87d0e654b418d910b
rBariles <rbariles@chattigo.com>
2022-05-12 08:51:40 -0300

    Prueba info camaras

--- src/app/components/configurations/scan/scan.component.ts
+++ src/app/components/configurations/scan/scan.component.ts
@@ -78,7 +78,7 @@ export class ScanComponent implements OnInit , AfterViewInit{
       if (videoDevices.length > 0) {
         let choosenDev;
         for (const dev of videoDevices) {
-          if (dev.label.includes('front')) {
+          if (dev.label.includes('back')) {
             choosenDev = dev;
             break;
           }
